c0f2e54424e508dea2833f84e8fa1db64bb0238f
JJ <juanfragamadrid@gmail.com>
2022-04-21 12:40:09 +0200

    Constructores clase

--- app/src/main/java/es/aplideka/myapplication/Clase.java
+++ app/src/main/java/es/aplideka/myapplication/Clase.java
@@ -2,8 +2,15 @@ package es.aplideka.myapplication;
 
 public class Clase
 {
+    private int _estado = 0;
+
     public Clase()
     {
 
+    }
+
+    public Clase(int _estado)
+    {
+        this._estado = _estado;
     }
 }
