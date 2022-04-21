f036baf63aec243360fff44ea495878df5c1f35b
JJ <juanfragamadrid@gmail.com>
2022-04-21 12:56:12 +0200

    Comentando cosas

--- app/src/main/java/es/aplideka/myapplication/Clase.java
+++ app/src/main/java/es/aplideka/myapplication/Clase.java
@@ -2,15 +2,15 @@ package es.aplideka.myapplication;
 
 public class Clase
 {
-    private int _estado = 0;
-
-    public Clase()
-    {
-
-    }
-
-    public Clase(int _estado)
-    {
-        this._estado = _estado;
-    }
+//    private int _estado = 0;
+//
+//    public Clase()
+//    {
+//
+//    }
+//
+//    public Clase(int _estado)
+//    {
+//        this._estado = _estado;
+//    }
 }
