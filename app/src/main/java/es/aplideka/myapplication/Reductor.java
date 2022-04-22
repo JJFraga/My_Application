db016436f8f96b5c159bb86923e41156f6be8ebd
JJ <juanfragamadrid@gmail.com>
2022-04-22 11:33:44 +0200

    Constructor y atributo "Reductor"

--- app/src/main/java/es/aplideka/myapplication/Reductor.java
+++ app/src/main/java/es/aplideka/myapplication/Reductor.java
@@ -2,7 +2,15 @@ package es.aplideka.myapplication;
 
 public class Reductor
 {
+    private int _id = 50;
+
     public Reductor()
     {
+
+    }
+
+    public Reductor(int id)
+    {
+        this._id = id;
     }
 }
